54d21989da9e65b1de469cd462200e6e74d7f1f6
dhanushramudri <ramudridhanush@gmail.com>
2024-04-26 22:22:33 +0530

    new commit 26 apr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Redux practice
+      <h4>
+        {" "}
+        Watched youtube videos codepen clone , redux , whatsapp clone , react
+        hooks (wds)
+      </h4>
     </div>
   );
 }
